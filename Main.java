43bbb1dbb3b11b641b4e50db258e6deac68918fd
Joao Pedro <devjonpedu@gmail.com>
2025-05-17 22:16:08 -0300

    implementa classe Scanner e atualiza método main para usar nova lógica de tokenização

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        String input = "8+5-7+9";
-        Parser p = new Parser (input.getBytes());
-        p.parse();
+    public static void main(String[] args)  {
+        String input = "4-8+6";
+        Scanner scan = new Scanner (input.getBytes());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
 
     }
 }
