bd7896dec05282dc6ddb07e783c2afe4b9d2986e
Joao Pedro <devjonpedu@gmail.com>
2025-05-17 23:10:09 -0300

    Suportando o tipo de token number - parte 2

--- Scanner.java
+++ Scanner.java
@@ -20,33 +20,36 @@ public class Scanner {
         }
     }
 
-    public String nextToken() {
+    public Token nextToken() {
         char ch = peek();
         if (ch == '0') {
             advance();
-            return Character.toString(ch);
+            return new Token(TokenType.NUMBER, Character.toString(ch));
         } else if (Character.isDigit(ch))
             return number();
 
         switch (ch) {
             case '+':
+                advance();
+                return new Token(TokenType.PLUS, "+");
             case '-':
                 advance();
-                return Character.toString(ch);
+                return new Token(TokenType.MINUS, "-");
+            case '\0':
+                return new Token(TokenType.EOF, "EOF");
             default:
-                break;
+                throw new Error("lexical error at " + ch);
         }
-
-        throw new Error("lexical error");
     }
 
-    private String number() {
+    private Token number() {
         int start = current;
         while (Character.isDigit(peek())) {
             advance();
         }
 
         String n = new String(input, start, current - start);
-        return n;
+        return new Token(TokenType.NUMBER, n);
     }
+
 }
